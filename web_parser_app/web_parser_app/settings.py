4da0ddc41e15cbcf1df3370a8563465172c72715
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-07-13 10:10:14 +0400

    celery basic set up

--- web_parser_app/web_parser_app/settings.py
+++ web_parser_app/web_parser_app/settings.py
@@ -137,3 +137,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#celery
+CELERY_TIMEZONE = "UTC"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60

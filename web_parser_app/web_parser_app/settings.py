2bd8ed57d48d02e46ed3b71667317f15e03b72c3
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-07-16 14:30:41 +0400

    change setting for media

--- web_parser_app/web_parser_app/settings.py
+++ web_parser_app/web_parser_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',
+    'parser_service',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +135,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
